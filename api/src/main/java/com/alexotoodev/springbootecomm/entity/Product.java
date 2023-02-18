753274ef10ebb0896d495f0c52d680b4a9e9b903
Alex Otoo <alexotoous@outlook.com>
2023-02-18 13:11:01 -0500

    first commit

---
+++ api/src/main/java/com/alexotoodev/springbootecomm/entity/Product.java
@@ -0,0 +1,64 @@
+package com.alexotoodev.springbootecomm.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+//with JPA annotations
+@Entity
+@Table(name = "product")
+// with lombok Data annotations
+@Data
+public class Product {
+    // with hibernate annotations
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date date_created;
+
+    @Column(name = "last-updated")
+    @UpdateTimestamp
+    private Date last_updated;
+
+}
---
+++ api/src/test/java/com/alexotoodev/springbootecomm/SpringBootEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexotoodev.springbootecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
